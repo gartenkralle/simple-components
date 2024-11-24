41a28f583b6468a9792416f6192d64ae33427d01
Clemens Schneider <clemens.schneider.ch@gmail.com>
2024-11-24 21:57:15 +0100

    Update

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import { loadTable } from "/table.js";
 
-const headerData = ['Name', 'Email', 'Action'];
+const headerData = ['Name', 'Email', 'Salary'];
 const jsonData = await (await fetch('customers.json')).json();
 const targetElement = document.getElementById('customers');
 

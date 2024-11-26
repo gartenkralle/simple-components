174b7a570a41257abd48c012b8d1795331605b55
Clemens Schneider <clemens.schneider.ch@gmail.com>
2024-11-26 10:44:58 +0100

    Update

--- main.js
+++ main.js
@@ -2,14 +2,14 @@ import { loadTable } from "/components/table.js";
 {
   const headerData = ["Name", "Email", "Salary"];
   const jsonData = await (await fetch("customers.json")).json();
-  const targetElement = document.getElementById("customerTable");
+  const targetElement = document.getElementById("customer-table");
 
   await loadTable(jsonData, headerData, targetElement);
 }
 
 import { loadModal } from "/components/modal.js";
 {
-  const sourceElement = document.getElementById("modalButton");
+  const sourceElement = document.getElementById("modal-button");
   const targetElement = document.getElementById("myModal");
 
   loadModal(sourceElement, targetElement);

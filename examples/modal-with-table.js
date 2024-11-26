640c908b0ac8d2519fd9244a2430094a48cdc804
Clemens Schneider <clemens.schneider.ch@gmail.com>
2024-11-26 17:09:27 +0100

    Update

--- examples/modal-with-table.js
+++ examples/modal-with-table.js
@@ -3,11 +3,11 @@ import { Modal } from "/components/modal.js";
 
 const headerData = ["Name", "Email", "Salary"];
 const jsonData = await (await fetch("customers.json")).json();
-const tableModal = document.getElementById("table-modal");
-const tableModalButton = document.getElementById("table-modal-button");
+const targetElement = document.getElementById("table-modal");
+const sourceElement = document.getElementById("table-modal-button");
 
 const table = new Table();
-table.load(jsonData, headerData, tableModal);
+table.load(jsonData, headerData, targetElement);
 
 const modal = new Modal();
-modal.load(tableModalButton, tableModal);
+modal.load(sourceElement, targetElement);

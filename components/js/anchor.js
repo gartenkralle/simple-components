85ade19c9a0a2e9be8cd8af18afca568db8c9556
Clemens Schneider <clemens.schneider.ch@gmail.com>
2025-02-09 14:41:16 +0100

    inline style for anchor and textbox

--- components/js/anchor.js
+++ components/js/anchor.js
@@ -3,7 +3,19 @@ export class Anchor extends HTMLElement {
         super();
 
         this.anchor = document.createElement("a");
-        this.anchor.className = "font-sans text-sky-600 no-underline hover:underline";
+        this.anchor.style = `
+            font-family: var(--font-family);
+            color: oklch(.588 .158 241.966);
+            text-decoration-line: none;`;
+
+        this.anchor.addEventListener("mouseenter", function(){
+            this.style.textDecorationLine = "underline";
+        });
+
+        this.anchor.addEventListener("mouseleave", function(){
+            this.style.textDecorationLine = "none";
+        });
+
         this.anchor.textContent = text;
         this.anchor.href = href;
 
--- components/js/textbox.js
+++ components/js/textbox.js
@@ -3,7 +3,36 @@ export class Textbox extends HTMLElement {
         super();
 
         this.textbox = document.createElement("input");
-        this.textbox.classList.add("sc-input");
+        this.textbox.style = `
+            font-family: var(--font-family);
+            width: 100%;
+            font-weight: 400;
+            font-size: 0.875rem;
+            line-height: 1.5rem;
+            padding: 6px 12px 6px 12px;
+            color: #333;
+            background-color: #fff;
+            border-radius: 5px;
+            border-width: 0px;
+            outline-color: #d1d5db;
+            outline-offset: -1px;
+            outline-style: solid;
+            outline-width: 1px;
+            box-sizing: border-box;`;
+
+        this.textbox.addEventListener("focus", function(){
+            this.style.outlineColor = "var(--primary-color)";
+            this.style.outlineOffset = "-2px";
+            this.style.outlineWidth = "2px";
+
+        });
+
+        this.textbox.addEventListener("blur", function(){
+            this.style.outlineColor = "#d1d5db";
+            this.style.outlineOffset = "-1px";
+            this.style.outlineWidth = "1px";
+        });
+
         this.textbox.type = "text";
 
         this.appendChild(this.textbox);

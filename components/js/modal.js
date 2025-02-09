ac9adfdb48309be50aeeda0e951e74c596a18cc3
Clemens Schneider <clemens.schneider.ch@gmail.com>
2025-02-09 09:59:40 +0100

    simplified modal nesting

--- components/js/modal.js
+++ components/js/modal.js
@@ -2,32 +2,29 @@ export class Modal extends HTMLElement {
     constructor() {
         super();
 
-        this.modal = document.createElement("div");
-        this.modal.classList.add("sc-modal");
+        this.classList.add("sc-modal");
 
         this.modalContent = document.createElement("div");
         this.modalContent.classList.add("sc-modal-content");
 
-        this.modal.appendChild(this.modalContent);
+        this.appendChild(this.modalContent);
 
-        this.modal.addEventListener("mousedown", (e) => {
-            if (e.target === this.modal) {
+        this.addEventListener("mousedown", (e) => {
+            if (e.target === this) {
                 this.hide();
             }
         });
 
-        this.modal.addEventListener("animationend", (e) => {
+        this.addEventListener("animationend", (e) => {
             if (e.animationName === "fadeIn") {
-                this.modal.dispatchEvent(new CustomEvent('shown'));
+                this.dispatchEvent(new CustomEvent('shown'));
             }
             else if (e.animationName === "fadeOut") {
-                this.modal.style.display = "none";
-                this.modal.classList.remove("sc-fade-out");
-                this.modal.dispatchEvent(new CustomEvent('hidden'));
+                this.style.display = "none";
+                this.classList.remove("sc-fade-out");
+                this.dispatchEvent(new CustomEvent('hidden'));
             }
         });
-
-        this.appendChild(this.modal);
     }
 
     add(htmlElement) {
@@ -35,16 +32,16 @@ export class Modal extends HTMLElement {
     }
 
     show() {
-        this.modal.style.display = "flex";
-        this.modal.classList.remove("sc-fade-out");
-        this.modal.classList.add("sc-fade-in");
-        this.modal.dispatchEvent(new CustomEvent('show'));
+        this.style.display = "flex";
+        this.classList.remove("sc-fade-out");
+        this.classList.add("sc-fade-in");
+        this.dispatchEvent(new CustomEvent('show'));
     }
 
     hide() {
-        this.modal.classList.remove("sc-fade-in");
-        this.modal.classList.add("sc-fade-out");
-        this.modal.dispatchEvent(new CustomEvent('hide'));
+        this.classList.remove("sc-fade-in");
+        this.classList.add("sc-fade-out");
+        this.dispatchEvent(new CustomEvent('hide'));
     }
 }
 
